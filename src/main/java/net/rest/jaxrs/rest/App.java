d945f4084da4cc2a75447d1db08db3c01a694c23
Somendra23 <somendra23@gmail.com>
2019-02-28 10:38:28 -0500

    jax rs

---
+++ src/main/java/net/rest/jaxrs/rest/App.java
@@ -0,0 +1,15 @@
+package net.rest.jaxrs.rest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Building JAX RS services in Dropwizard" );
+        
+        
+    }
+}
